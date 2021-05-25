1824000ec1d32b0c3655aaefa59f78f20286bc3d
Krzysztof <krzysztof.klich@edu.uekat.pl>
2021-05-26 00:07:54 +0200

    xd

---
+++ ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+
+namespace ContosoUniversity.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            // Commenting out LINQ to show how to do the same thing in SQL.
+            //IQueryable<EnrollmentDateGroup> = from student in db.Students
+            //           group student by student.EnrollmentDate into dateGroup
+            //           select new EnrollmentDateGroup()
+            //           {
+            //               EnrollmentDate = dateGroup.Key,
+            //               StudentCount = dateGroup.Count()
+            //           };
+
+            // SQL version of the above LINQ code.
+           
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+         
+            base.Dispose(disposing);
+        }
+    }
+}
